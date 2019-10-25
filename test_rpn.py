0bdaa7851c6b23a34f5a21e1beb2ec381e27880b
Anthony Zhu <aqzhu@umich.edu>
2019-10-25 14:42:40 -0400

    Implemented subtraction and subtraction test

--- test_rpn.py
+++ test_rpn.py
@@ -4,4 +4,7 @@ import rpn
 class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate('1 1 +')
-        self.assertEqual(2, result) #self is similar to 'this' in c++
+        self.assertEqual(2, result)
+    def test_sub(self):
+        result = rpn.calculate('5 3 -')
+        self.assertEqual(2, result)
